ed9934a3faf4dca8e33ad391c4abcab570afe3e1
Tre <94414702+Tre0207@users.noreply.github.com>
2023-11-19 20:16:40 -0500

    Update main.js for database in firebase

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css';
+import { getFirestore } from 'firebase/firestore'
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDg175AidURF6LpgHYZS3J8SQPVhn8dORc",
   authDomain: "senior-project-41ab5.firebaseapp.com",
+  databaseURL: "https://senior-project-41ab5-default-rtdb.firebaseio.com",
   projectId: "senior-project-41ab5",
   storageBucket: "senior-project-41ab5.appspot.com",
   messagingSenderId: "98340510353",
@@ -19,6 +21,13 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
 
-createApp(App).provide('auth', auth).mount('#app');
+// Create a Firestore instance
+const db = getFirestore(firebaseApp);
+const vueApp = createApp(App)
+
+// Make Firestore available globally within Vue app
+vueApp.config.globalProperties.$db = db
+
+vueApp.provide('auth', auth).mount('#app');
 
 export { auth };
